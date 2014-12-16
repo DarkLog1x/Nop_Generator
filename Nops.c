03504ee14b4e012aa8d63a80e197877602b2042d
Aleksander Okonski <aleksander.oko@gmail.com>
2014-12-15 23:02:45 -0800

    Simple Nop generator

---
+++ Nops.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char *argv[]){
+	
+	if(argc < 2){
+		printf("Add a number of characters!\n");
+		return 0;
+	}
+
+	char array[] = {'\x40', '\x43', '\x41', '\x42', '\x48', '\x4B', '\x49', '\x4A', '\x0f'};
+	srand(time(NULL));
+	
+	int i;
+	for(i = 0; i < atoi(argv[1]); i++){
+		int random = rand() % sizeof(array);
+		printf("0x%x", array[random]);
+	}
+
+	return 0;
+}
